e82e3587d0567cdf44074f96bdcc129e076f1366
ZacharyLyonMcRae <135399195+ZacharyLyonMcRae@users.noreply.github.com>
2023-07-12 16:53:43 -0400

    Update Driver.java
    Added my component

--- Project_July11th/src/Demo/Driver.java
+++ Project_July11th/src/Demo/Driver.java
@@ -9,30 +9,25 @@ import Package5.Mav;
 
 public class Driver extends Airplane {
 
-	public static void findLeastAndMostExpensiveUAV(Airplane[] flyingObjects) {
+public static void findLeastAndMostExpensiveUAV(Airplane[] flyingObjects) {
 		
-		// Created UAV variables
 		UAV leastExpensive = null;
 		UAV mostExpensive = null;
 
-		// for loop to find all instances of the UAV subclass
 		for (int i = 0; i < flyingObjects.length; i++) {
 			if (flyingObjects[i] instanceof UAV) {
 				UAV uav = (UAV) flyingObjects[i];
 
-				// Comparing all values at index i against all previous values and storing the lowest
 				if (leastExpensive == null || uav.getPrice() < leastExpensive.getPrice()) {
 					leastExpensive = uav;
 				}
 
-				// Comparing all values at index i against all previous values and storing the highest
 				if (mostExpensive == null || uav.getPrice() > mostExpensive.getPrice()) {
 					mostExpensive = uav;
 				}
 			}
 		}
 
-		// All possible outcomes printed
 		if (leastExpensive != null && mostExpensive != null) {
 			System.out.println("The least expensive UAV is:\n" + leastExpensive.toString());
 			System.out.println("The most Eexpensive UAV is:\n" + mostExpensive.toString());
@@ -45,6 +40,34 @@ public class Driver extends Airplane {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Airplane[] flyingObjects = { new Helicopter ("Textron", 1900000, 750, 6, 2018, 10), new Helicopter("Tetxtron", 1700000, 800, 6, 2014, 8), new UAV(150.00, 180000.00), 
+				new UAV(200.00, 250000.00), new Airplane("Boeing", 2000000, 30000), new Airplane("Boeing", 2200000, 34000), new Quadcopter("Textron", 2400000, 900, 8, 2020, 12, 200),
+				new Quadcopter("AMS", 2200000, 880, 8, 2021, 14, 240), new Multirotor("DSL", 2800000, 900, 16, 2018, 20, 12), new Multirotor("FA", 2400000, 960, 12, 2015, 14, 12),
+				new Airplane("QTR", 1800000, 32000), new UAV(300.00, 150000.00), new AgriculturalDrone(40, 2000, "GeekTech", 40), new AgriculturalDrone(36, 2400, "Laurence", 32),
+				new Mav(10, 2200, "E-620", 30), new Mav(12, 2400, "B-450", 36), new UAV(220.00, 240000.00), new Airplane("Strelock", 2600000, 40000), 
+				new Mav(14, 1900, "Q-7200", 28), new Airplane("Archer", 300000, 40000)};
+
+		Airplane[] flyingObjects2 = {new Helicopter ("Textron", 1900000, 750, 6, 2018, 10), new Helicopter("Tetxtron", 1700000, 800, 6, 2014, 8), new Airplane("Boeing", 2000000, 30000), 
+				new Airplane("Boeing", 2200000, 34000), new Quadcopter("Textron", 2400000, 900, 8, 2020, 12, 200), new Quadcopter("AMS", 2200000, 880, 8, 2021, 14, 240), 
+				new Multirotor("DSL", 2800000, 900, 16, 2018, 20, 12), new Multirotor("FA", 2400000, 960, 12, 2015, 14, 12), new Airplane("QTR", 1800000, 32000), 
+				new AgriculturalDrone(40, 2000, "GeekTech", 40), new AgriculturalDrone(36, 2400, "Laurence", 32), new Mav(10, 2200, "E-620", 30), new Mav(12, 2400, "B-450", 36), 
+				new Airplane("Strelock", 2600000, 40000), new Mav(14, 1900, "Q-7200", 28), new Airplane("Archer", 300000, 40000)};
+		
+
+        System.out.println("Testing equality of objects:");
+        
+        System.out.println("Are flyingObjects[0] and flyingObjects[2] equal? " +
+                flyingObjects[0].equals(flyingObjects[2]));
+        
+        System.out.println("Are flyingObjects[4] and flyingObjects[5] equal? " +
+                flyingObjects[4].equals(flyingObjects[5]));
+        
+        // Two objects with similar values
+        System.out.println("Are flyingObjects[1] and flyingObjects[6] equal? " +
+                flyingObjects[1].equals(flyingObjects[6]));
+		
+		findLeastAndMostExpensiveUAV(flyingObjects);
 
 	}
 
